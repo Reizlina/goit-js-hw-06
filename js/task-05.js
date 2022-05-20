9ececb8ff4d508b50f0f935f0993f99ec1b4544d
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-05-20 20:58:06 +0300

    Fixed bugs

--- js/task-05.js
+++ js/task-05.js
@@ -5,4 +5,9 @@ const output = document.querySelector('#name-output');
 
 inputForm.addEventListener('input', event => {
   output.textContent = event.target.value;
+  if (event.target.value.length > 0) {
+    output.innerHTML = event.target.value;
+    return;
+  }
+  output.innerHTML = 'Anonymous';
 });
--- js/task-09.js
+++ js/task-09.js
@@ -4,7 +4,9 @@ function getRandomHexColor() {
 
 const btn = document.querySelector('.change-color');
 const bodyColor = document.querySelector('body');
+const colorName = document.querySelector('.color');
 
-btn.addEventListener('click', () => {
+const randomColor = btn.addEventListener('click', () => {
   bodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+  colorName.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 });
